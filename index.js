daff0b3827da53807d067dcb0b70302ff0a54540
PatelPratik39 <ptl.pratik717@gmail.com>
2024-06-18 01:34:39 -0500

    Initial Commit

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 
 const app = express();
-const PORT = 6060;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send({ Hello: "There" });
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is running at ${PORT}`);
